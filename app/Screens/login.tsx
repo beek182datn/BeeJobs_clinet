482ac288e0f29e50b55b5dae532f0d41d90dfee3
DESKTOP-8446ASD\TienND <nguyenduytienbgg3@gmail.com>
2024-06-05 16:45:02 +0700

    update khi vao login ko quay lai duoc man hinh chao

--- app/Screens/login.tsx
+++ app/Screens/login.tsx
@@ -1,8 +1,34 @@
 // app/login.tsx
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React,{useEffect, useState} from 'react';
+import { View, Text, StyleSheet , ToastAndroid} from 'react-native';
+import { BackHandler } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 const Login = () => {
+  const navigation = useNavigation();
+  const [backPressedOnce, setBackPressedOnce] = useState(false);
+  useEffect(() => {
+    const handleBackPress = () => {
+      if (backPressedOnce) {
+        BackHandler.exitApp();
+       
+      }else{
+       ToastAndroid.show('Nhấn back một lần nữa để thoát', ToastAndroid.SHORT);
+       setBackPressedOnce(true);
+       // Reset trạng thái đã nhấn nút back sau 2 giây
+       setTimeout(() => setBackPressedOnce(false), 2000);
+      }
+
+      return true;
+     
+    };
+
+    // Đăng ký trình xử lý sự kiện nút back khi màn hình đăng nhập được hiển thị
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    // Hủy đăng ký trình xử lý sự kiện khi màn hình đăng nhập bị hủy
+    return () => backHandler.remove();
+  }, [backPressedOnce]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Màn hình Đăng nhập</Text>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {NavigationContainer, DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -28,15 +28,15 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-     {/* <Stack.Screen name="login" options={{ headerShown: false }} /> */}
         <Stack.Screen name="Screens/login" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
 }
+
 
 
 
