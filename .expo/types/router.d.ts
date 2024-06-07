bb2f93f79e511eb4d2d0b5037c868a61ec6d5870
DESKTOP-8446ASD\TienND <nguyenduytienbgg3@gmail.com>
2024-06-07 14:18:49 +0700

    save

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/..\components\AlertComponent` | `/RegisterScreen` | `/_sitemap`;
+  type StaticRoutes = `/` | `/RegisterScreen` | `/_sitemap`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
