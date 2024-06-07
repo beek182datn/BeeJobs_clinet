dd8be5a362067b4a097cd5b81a3a7e75cfcfef22
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-06-07 14:02:46 +0700

    Add initial component and styles

---
+++ app/LoginScreen.tsx
@@ -0,0 +1,37 @@
+// LoginScreen.tsx
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import axios, { AxiosResponse } from "axios";
+import { useRouter } from 'expo-router';
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Đăng nhập</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginBottom: 30,
+  },
+});
+
+export default LoginScreen;
