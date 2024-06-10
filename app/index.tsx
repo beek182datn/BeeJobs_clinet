865b6b4325d15dc479e9a3c11238ec868958c134
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-06-10 23:21:26 +0700

    Thay đổi ở index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
 
     useEffect(()=>{
       const timer = setTimeout(()=>{
-        router.push("RegisterScreen")
+        router.push("LoginScreen");
       }, 3000);
       return () => clearTimeout(timer);
     }, [router])
