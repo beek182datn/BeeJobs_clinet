56d71baed37db5538839c03b09c17bf15af710db
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-06-11 13:27:08 +0700

    Update xác thực OTP thành 6 ký tự

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
       <Stack.Screen name="LoginScreen" options={{headerShown: false}}/>
       <Stack.Screen name="RegisterScreen" options={{headerShown: false}}/>
       <Stack.Screen name="OtpVerificationScreen" options={{headerShown: false}}/>
+      <Stack.Screen name="ResetPasswordScreen" options={{headerShown: false}}/>
       <Stack.Screen name="(homes)" options={{headerShown: false}}/>
     </Stack>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     const router = useRouter();
     useEffect(()=>{
       const timer = setTimeout(()=>{
-        router.push("LoginScreen");
+        router.push("LoginScreen",);
       }, 3000);
       return () => clearTimeout(timer);
     }, [router])
