01925c0a3976b2e52cdbcee859998769270abc54
huylvph30524 <vhuy887@gmail.com>
2024-06-11 09:58:33 +0700

    Add notification to registration screen

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/(homes)` | `/ForgotPasswordScreen` | `/Home` | `/LoginScreen` | `/OtpVerificationScreen` | `/Profile` | `/RegisterScreen` | `/_sitemap`;
+  type StaticRoutes = `/` | `/(homes)` | `/ForgotPasswordScreen` | `/Home` | `/LoginScreen` | `/OtpVerificationScreen` | `/Profile` | `/RegisterScreen` | `/ResetPasswordScreen` | `/_sitemap`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
