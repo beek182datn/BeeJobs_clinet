2401f52b05c7382f77319c41f0b2743bf156b4cd
longnick2001 <129032558+longnick2001@users.noreply.github.com>
2024-06-12 08:38:38 +0700

    long update about jobslist in home

--- app/(homes)/Home.tsx
+++ app/(homes)/Home.tsx
@@ -1,15 +1,29 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'expo-router';
 import { useNavigation } from '@react-navigation/native'
+import JobsList from '@/components/list_obj/JobsList';
+import { Job } from '@/components/Model';
+import { fetchJobs } from '@/components/fetch_data/api';
 
 const Home = () => {
   const router = useRouter();
+  const [jobs, setJobs] = useState<Job[]>([]);
+  useEffect(() => {
+    const loadJobs = async () => {
+      const fetchedJobs = await fetchJobs();
+      setJobs(fetchedJobs);
+      console.log(jobs);
+    };
+    loadJobs();
+  }, []);
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.styleButton} onPress={() => router.push('LoginScreen')}>
         <Text style={styles.styleButtonText}>Đăng xuất</Text>
       </TouchableOpacity>
+
+      <JobsList jobs={jobs}/>
     </View>
   )
 }
