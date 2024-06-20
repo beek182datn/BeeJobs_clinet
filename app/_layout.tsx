d65d7929f4d8dbdb57a05e18b83ace729aa52454
longnick2001 <129032558+longnick2001@users.noreply.github.com>
2024-06-20 09:21:43 +0700

    long update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
-import { Stack } from "expo-router";
+import { Navigator, Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack>
-       <Stack.Screen name="CompleteProfileScreen" options={{headerShown: false}}/>
-       <Stack.Screen name="CompleteProfileScreen1" options={{headerShown: false}}/>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
-      <Stack.Screen name="ForgotPasswordScreen" options={{headerShown: false}}/>
-      <Stack.Screen name="LoginScreen" options={{headerShown: false}}/>
-      <Stack.Screen name="RegisterScreen" options={{headerShown: false}}/>
-      <Stack.Screen name="OtpVerificationScreen" options={{headerShown: false}}/>
-      <Stack.Screen name="ResetPasswordScreen" options={{headerShown: false}}/>
-      <Stack.Screen name="(homes)" options={{headerShown: false}}/>
+      <Stack.Screen name="CompleteProfileScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="CompleteProfileScreen1" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="ForgotPasswordScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="RegisterScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="OtpVerificationScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="ResetPasswordScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="(homes)" options={{ headerShown: false }} />
     </Stack>
   );
 }
