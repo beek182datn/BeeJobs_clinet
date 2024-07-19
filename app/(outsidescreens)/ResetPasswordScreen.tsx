c10dd02891977b6efa514eb2b4395babff99858c
huylvph30524 <vhuy887@gmail.com>
2024-07-19 12:45:24 +0700

    huy update

--- app/(outsidescreens)/ResetPasswordScreen.tsx
+++ app/(outsidescreens)/ResetPasswordScreen.tsx
@@ -1,67 +1,54 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
-import axios from "axios";
-import { useRouter } from 'expo-router';
+import React, { useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, BackHandler } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; // Thêm thư viện cho icon (nếu sử dụng)
+import { useRouter } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
 
-const ResetPasswordScreen: React.FC = () => {
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+const ResetPasswordScreen = () => {
   const router = useRouter();
+  useEffect(() => {
+    const backAction = () => {
+      router.replace("Profile");
+      return true;
+    };
 
-  const handleResetPassword = async () => {
-    if (password !== confirmPassword) {
-      alert("Mật khẩu xác nhận không khớp.");
-      return;
-    }
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
 
-    try {
-      const response = await axios.post('http://beejobs.io.vn:14307/api/reset-password', {
-        password: password,
-      });
-      console.log('Đặt lại mật khẩu thành công:', response.data);
-      alert("Mật khẩu của bạn đã được đặt lại thành công.");
-      setPassword("");
-      setConfirmPassword("");
-      router.push('LoginScreen'); // Điều hướng đến màn hình đăng nhập
-    } catch (error) {
-      console.error('Lỗi đặt lại mật khẩu:', error);
-      alert("Đã xảy ra lỗi trong quá trình đặt lại mật khẩu. Vui lòng thử lại sau.");
-    }
-  };
+    return () => backHandler.remove();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Đặt lại mật khẩu</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Mật khẩu mới"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
+      <View style={styles.header}>
+          {/* <Icon name="arrow-left" size={20} color="#000" onPress={router.back} /> */}
+          <TouchableOpacity onPress={router.back}
+            style={{ backgroundColor: '#2196F3', borderRadius: 30, padding: 5 }}>
+            <Ionicons name="arrow-back" size={22} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Đổi mật khẩu</Text>
+        </View>
+      <View style={styles.form}>
+        <Text style={styles.label}>Email đăng nhập</Text>
+        <TextInput style={styles.input} value="vhuy887@gmail.com" editable={false} />
+        
+        <Text style={styles.label}>Mật khẩu hiện tại</Text>
+        <TextInput style={styles.input} placeholder="Nhập mật khẩu hiện tại" secureTextEntry />
+        
+        <Text style={styles.label}>Mật khẩu mới</Text>
+        <TextInput style={styles.input} placeholder="Nhập mật khẩu mới" secureTextEntry />
+        
+        <Text style={styles.label}>Nhập lại mật khẩu mới</Text>
+        <TextInput style={styles.input} placeholder="Nhập lại mật khẩu mới" secureTextEntry />
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Xác nhận mật khẩu"
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          secureTextEntry
-        />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonCancel} onPress={router.back}>
+          <Text style={styles.buttonText}>Hủy</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonSave}>
+          <Text style={styles.buttonText}>Lưu</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
-        <Text style={styles.buttonText}>Đặt lại mật khẩu</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.backButton} onPress={() => router.push('LoginScreen')}>
-        <Text style={styles.backButtonText}>Quay lại đăng nhập</Text>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -69,54 +56,77 @@ const ResetPasswordScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    padding: 20,
-    justifyContent: "center",
+    backgroundColor: '#f5f5f5',
+    marginTop: 20,
+    position: 'relative',
   },
-  title: {
-    fontSize: 30,
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  
+  headerTitle: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 18,
     fontWeight: "bold",
-    alignSelf: "center",
-    marginBottom: 30,
   },
-  inputContainer: {
-    borderWidth: 1,
-    borderColor: "#A9A9A9",
-    borderRadius: 25,
-    marginBottom: 20,
-    paddingHorizontal: 15,
-    backgroundColor: "#f9f9f9",
-    padding: 10,
+  form: {
+    padding: 16,
+    backgroundColor: '#fff',
+    marginTop: 16,
+    borderRadius: 8,
+    marginHorizontal: 16,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
   },
   input: {
-    padding: 10,
-    fontSize: 16,
-    color: "#000",
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    backgroundColor: '#f9f9f9',
   },
-  button: {
-    backgroundColor: "#007BFF",
-    paddingVertical: 15,
-    borderRadius: 25,
-    alignItems: "center",
-    marginBottom: 20,
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  buttonCancel: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 12,
+    backgroundColor: '#ccc',
+    borderRadius: 4,
+    marginRight: 8,
+  },
+  buttonSave: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 12,
+    backgroundColor: '#007AFF',
+    borderRadius: 4,
+    marginLeft: 8,
   },
   buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  backButton: {
-    backgroundColor: "#f9f9f9",
-    paddingVertical: 15,
-    borderRadius: 25,
-    alignItems: "center",
-    borderColor: "#007BFF",
-    borderWidth: 1,
-  },
-  backButtonText: {
-    color: "#007BFF",
-    fontSize: 18,
-    fontWeight: "bold",
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,91 +1,258 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useBackHandler } from "../../components/BackHandler";
-import AlertComponent from "@/components/AlertComponent";
-import { getUserInfo, findWorkerById } from '@/components/fetch_data/api';
+import React, { useRef, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Switch,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+  Animated,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import ActionSheet from "react-native-actionsheet";
 import { useRouter } from 'expo-router';
-import { User, Worker } from "../../components/Model/Model";
+import * as DocumentPicker from 'expo-document-picker';
+import Modal from 'react-native-modal';
+import { Picker } from '@react-native-picker/picker';
 
-const Profile = () => {
+const Profile: React.FC = () => {
+  const actionSheetRef = useRef<ActionSheet>(null);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
   const router = useRouter();
-  const [user, setUser] = useState<User | null>();
-  const [worker, setWorker] = useState<Worker | null>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedYear, setSelectedYear] = useState("1 năm");
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
 
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        // const emailValue = await AsyncStorage.getItem('userProfile');
-        // console.log(emailValue)
-        const user: User | null = await getUserInfo();
-        setUser(user);
-        if (user) {
-          const worker = await findWorkerById(user.id_user);
-          setWorker(worker);
-        }
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    };
+    Animated.parallel([
+      Animated.spring(fadeAnim, {
+        toValue: 1,
+        friction: 4,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [fadeAnim]);
 
-    fetchUserInfo();
-  }, []);
+  const handleLogoutPress = () => {
+    Alert.alert(
+      'Xác nhận đăng xuất',
+      'Bạn có chắc chắn muốn đăng xuất?',
+      [
+        {
+          text: 'Hủy',
+          style: 'cancel',
+        },
+        {
+          text: 'Đăng xuất',
+          onPress: () => {router.push('/LoginScreen')},
+        },
+      ],
+      { cancelable: false }
+    );
+  };
 
-  const handleLogout = async () => {
-    try {
-      await AsyncStorage.removeItem('userProfile');
-      setUser(null);
-      router.push('LoginScreen')
-    } catch (error) {
-      console.error('Error logging out:', error);
+  const showActionSheet = () => {
+    if (actionSheetRef.current) {
+      actionSheetRef.current.show();
     }
   };
 
-  const handleUpdateInfo = () => {
-    router.push('/CompleteProfileScreen1');
-  }
+  const handleActionSheetPress = (index: number) => {
+    switch (index) {
+      case 0:
+        //Alert.alert('Chụp ảnh');
+        break;
+      case 1:
+        DocumentPicker.getDocumentAsync({
+          type: '*/*', // Allow any file type, you can specify MIME types if needed
+          copyToCacheDirectory: true
+        })
+          .then((response) => {
+            if (2>0) {
+              console.log('Selected file: ', response);
+              // Process the selected file here
+            } else {
+              console.log('User cancelled the picker');
+            }
+          })
+          .catch((err) => {
+            console.error('DocumentPicker Error: ', err);
+          });
+        break;
+      case 2:
+        //Alert.alert('Xóa ảnh đại diện');
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      {worker &&
-        <>
-          <View style={styles.profileHeader}>
-            <Image source={{ uri: worker?.worker_avatar }} style={styles.avatar} />
-            <Text style={styles.name}>{worker?.worker_name}</Text>
-          </View>
-          <View style={styles.profileInfo}>
-            <Text style={styles.infoLabel}>Phone:</Text>
-            <Text style={styles.infoValue}>{worker?.phone}</Text>
-            <Text style={styles.infoLabel}>Email:</Text>
-            <Text style={styles.infoValue}>{worker?.email}</Text>
-          </View>
-        </>
-      }
+    <ScrollView style={styles.container}>
+      <View style={styles.profileHeader}>
+        <TouchableOpacity onPress={showActionSheet}>
+          <Image
+            source={{ uri: "https://via.placeholder.com/100" }}
+            style={styles.avatar}
+          />
+        </TouchableOpacity>
+        <Text style={styles.name}>Lê Văn Huy</Text>
+        <Text style={styles.candidateId}>Email: vhuy887@gmail.com</Text>
+        <TouchableOpacity style={styles.upgradeButton}>
+          <Text style={styles.upgradeText}>Nâng cấp tài khoản</Text>
+        </TouchableOpacity>
+      </View>
 
-      {user !== null &&
-        <>
-          <TouchableOpacity style={styles.updateButton} onPress={handleUpdateInfo}>
-            <Text style={styles.updateText}>Hoàn thiện hồ sơ</Text>
+      <View style={styles.section}>
+        <View style={styles.row}>
+          <Text style={styles.title}>Kinh nghiệm làm việc</Text>
+          <TouchableOpacity onPress={toggleModal}>
+            <Text style={styles.editText}>Sửa</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-            <Text style={styles.logoutText}>Đăng xuất</Text>
+        </View>
+        <Text style={styles.subtitle}>Chưa cập nhật</Text>
+      </View>
+
+      <View style={styles.section}>
+        <View style={styles.row}>
+          <Text style={styles.title}>Công việc mong muốn</Text>
+          <TouchableOpacity>
+            <Text style={styles.editText}>Sửa</Text>
           </TouchableOpacity>
-        </>
-      }
-    </View>
+        </View>
+        <Text style={styles.subtitle}>Chưa cập nhật</Text>
+      </View>
+
+      <View style={styles.section}>
+        <View style={styles.row}>
+          <Text style={styles.title}>Địa điểm làm việc mong muốn</Text>
+          <TouchableOpacity>
+            <Text style={styles.editText}>Sửa</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.subtitle}>Chưa cập nhật</Text>
+      </View>
+
+      <View style={styles.jobManagement}>
+        <Text style={styles.managementTitle}>Quản lý tìm việc</Text>
+        <View style={styles.row}>
+          <View style={styles.managementBox}>
+            <Ionicons name="briefcase" size={30} color="#0099CC" />
+            <Text style={styles.managementText}>Việc làm đã ứng tuyển</Text>
+          </View>
+          <View style={styles.managementBox}>
+            <Ionicons name="bookmark" size={30} color="#0099CC" />
+            <Text style={styles.managementText}>Việc làm đã lưu</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.section}>
+        <View style={styles.row}>
+          <View style={styles.infoBox}>
+            <Ionicons name="checkmark-circle" size={30} color="#0099CC" />
+            <Text style={styles.infoText}>Việc làm phù hợp</Text>
+            <Text style={styles.infoNumber}>0</Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Ionicons name="business" size={30} color="#0099CC" />
+            <Text style={styles.infoText}>Công ty đang theo dõi</Text>
+            <Text style={styles.infoNumber}>0</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.infoBox}>
+            <Ionicons name="eye" size={30} color="#0099CC" />
+            <Text style={styles.infoText}>NTD đã xem hồ sơ</Text>
+            <Text style={styles.infoNumber}>0</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.section}>
+        <TouchableOpacity style={styles.banner}>
+          <Text style={styles.bannerText}>Khám phá việc làm gần bạn</Text>
+          <Text style={styles.bannerButton}>XEM NGAY</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.utilitiesTitle}>Tiện ích</Text>
+        <TouchableOpacity style={styles.utilityItem}>
+          <Ionicons name="document" size={30} color="#0099CC" />
+          <Text style={styles.utilityText}>Hướng dẫn viết CV</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.accountSettingsTitle}>Cài đặt tài khoản</Text>
+        <TouchableOpacity style={styles.utilityItem} onPress={() => router.push("ResetPasswordScreen")}>
+          <Ionicons name="key" size={30} color="#0099CC" />
+          <Text style={styles.utilityText}>Đổi mật khẩu</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.utilityItem}>
+          <Ionicons name="lock-closed" size={30} color="#0099CC" />
+          <Text style={styles.utilityText}>Vô hiệu hóa tài khoản</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogoutPress}>
+        <Text style={styles.logoutText}>Đăng xuất</Text>
+        <Ionicons name="log-out-outline" size={20} color="#333" />
+      </TouchableOpacity>
+
+      <ActionSheet
+        ref={actionSheetRef}
+        style={styles.actionsheet}
+        title={"Cập nhật ảnh đại diện"}
+        options={["Chụp ảnh", "Chọn từ thư viện", "Xóa ảnh đại diện", "Đóng"]}
+        cancelButtonIndex={3}
+        destructiveButtonIndex={2}
+        onPress={handleActionSheetPress}
+      />
+      <Modal 
+        isVisible={isModalVisible}
+        onBackdropPress={toggleModal}
+        style={styles.modal}
+      >
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Chọn số năm đi làm</Text>
+          <Picker
+            selectedValue={selectedYear}
+            onValueChange={(itemValue) => setSelectedYear(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Sắp đi làm" value="Sắp đi làm" />
+            <Picker.Item label="Dưới 1 năm" value="Dưới 1 năm" />
+            <Picker.Item label="1 năm" value="1 năm" />
+            <Picker.Item label="2 năm" value="2 năm" />
+            <Picker.Item label="3 năm" value="3 năm" />
+            <Picker.Item label="4 năm" value="4 năm" />
+          </Picker>
+          <TouchableOpacity onPress={toggleModal}>
+            <Text style={styles.doneText}>Xong</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingHorizontal: 20,
-    paddingVertical: 30,
+    backgroundColor: "#fff",
   },
   profileHeader: {
-    alignItems: 'center',
-    marginBottom: 20,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
   },
   avatar: {
     width: 100,
@@ -95,56 +262,193 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  profileInfo: {
-    backgroundColor: 'white',
-    borderRadius: 8,
-    padding: 20,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginBottom: 20,
+  candidateId: {
+    fontSize: 14,
+    color: "gray",
   },
-  infoLabel: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 4,
-  },
-  infoValue: {
-    fontSize: 16,
-    color: '#666',
-    marginBottom: 8,
-  },
-  updateButton: {
-    backgroundColor: '#007AFF',
-    paddingVertical: 12,
+  upgradeButton: {
+    marginTop: 10,
+    paddingVertical: 5,
     paddingHorizontal: 20,
-    borderRadius: 8,
-    alignItems: 'center',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#0099CC",
+  },
+  upgradeText: {
+    color: "#0099CC",
+  },
+  section: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  editText: {
+    color: "#0099CC",
+  },
+  subtitle: {
+    marginTop: 5,
+    fontSize: 14,
+    color: "orange",
+  },
+  jobManagement: {
+    padding: 15,
+  },
+  managementTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
     marginBottom: 10,
   },
-  updateText: {
-    color: 'white',
+  managementBox: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  managementText: {
+    marginTop: 5,
+    fontSize: 14,
+    color: "gray",
+  },
+  infoBox: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+    marginHorizontal: 5,
+    marginBottom: 30,
+  },
+  infoText: {
+    marginTop: 5,
+    fontSize: 14,
+    color: "gray",
+  },
+  infoNumber: {
+    marginTop: 5,
+    fontSize: 14,
+    color: "#0099CC",
+  },
+  banner: {
+    backgroundColor: "#0099CC",
+    padding: 20,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  bannerText: {
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  bannerButton: {
+    color: "#fff",
+    fontSize: 14,
+    marginTop: 10,
+  },
+  utilitiesTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  utilityItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  utilityText: {
+    fontSize: 14,
+    marginLeft: 10,
+  },
+  accountSettingsTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  settingItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+  settingText: {
+    fontSize: 14,
+    marginLeft: 10,
   },
   logoutButton: {
-    backgroundColor: '#e74c3c',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderColor: "#ddd",
+    borderWidth: 1,
     borderRadius: 8,
-    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    marginBottom: 20,
   },
   logoutText: {
+    marginRight: 10,
+    fontSize: 16,
+    color: "#333",
+  },
+  actionsheet: {
+    margin: 20,
+  },
+  fadingContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "powderblue",
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "blue",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#007bff',
+    borderRadius: 5,
+  },
+  buttonText: {
     color: 'white',
     fontSize: 16,
+  },
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  picker: {
+    height: 150,
+  },
+  doneText: {
+    textAlign: 'right',
+    color: '#007bff',
+    fontSize: 16,
+    marginTop: 10,
   },
 });
 
