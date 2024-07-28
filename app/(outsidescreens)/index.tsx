ff2fc331640dc528b8d5ee7ff8193549d712d54e
longnick2001 <129032558+longnick2001@users.noreply.github.com>
2024-07-28 08:18:43 +0700

    ok

--- app/(outsidescreens)/index.tsx
+++ app/(outsidescreens)/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   const router = useRouter();
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.navigate("/Home");
+      router.navigate("/LoginScreen");
     }, 2000);
     return () => clearTimeout(timer);
   }, [router])
