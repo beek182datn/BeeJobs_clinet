f6adb23383bab2398adc9d935d6f3ef2343b0faa
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-08-09 10:31:10 +0700

    Phần 4: Thêm mục Điều kiện Sử dụng

--- app/(outsidescreens)/TermsOfService.tsx
+++ app/(outsidescreens)/TermsOfService.tsx
@@ -12,6 +12,13 @@ export default function TermsOfService() {
           BeeJobs cung cấp nền tảng trực tuyến để kết nối người tìm việc với nhà tuyển dụng. Chúng tôi cung cấp các công cụ và tính năng để người dùng có thể tạo hồ sơ, tìm kiếm việc làm, và tương tác với nhà tuyển dụng. BeeJobs không can thiệp vào quá trình tuyển dụng của từng bên, và không chịu trách nhiệm về các giao dịch diễn ra giữa người dùng và nhà tuyển dụng.
         </Text>
       </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>2. Điều kiện Sử dụng</Text>
+        <Text style={styles.sectionContent}>
+          Người dùng phải cung cấp thông tin chính xác và hoàn chỉnh khi đăng ký tài khoản và sử dụng dịch vụ của chúng tôi. Người dùng phải tuân thủ các quy định về bảo mật thông tin cá nhân và không được sử dụng dịch vụ BeeJobs cho các mục đích vi phạm pháp luật. BeeJobs có quyền ngừng cung cấp dịch vụ hoặc khóa tài khoản của người dùng nếu phát hiện vi phạm điều khoản dịch vụ.
+        </Text>
+      </View>
     </ScrollView>
   );
 }
