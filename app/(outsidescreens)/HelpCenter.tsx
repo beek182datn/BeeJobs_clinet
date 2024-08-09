13599b4646077444e6fa1aadaa6e1afe6962bdbf
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-08-09 10:27:42 +0700

    Phần 4: Thêm mục Câu hỏi thường gặp (FAQs)

--- app/(outsidescreens)/HelpCenter.tsx
+++ app/(outsidescreens)/HelpCenter.tsx
@@ -13,6 +13,14 @@ export default function HelpCenter() {
           <Text style={styles.link} onPress={() => openLink('https://github.com/beek182datn/BeeJobs_Client_Companies/commit/d03bceeda0236fcff4538c8f071e08e9491f9500?diff=unified&w=0')}>Hướng dẫn sử dụng</Text>
         </Text>
       </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Câu hỏi thường gặp (FAQs)</Text>
+        <Text style={styles.sectionContent}>
+          Các câu hỏi thường gặp về BeeJobs và câu trả lời của chúng tôi có thể được tìm thấy tại đây: {' '}
+          <Text style={styles.link} onPress={() => openLink('https://chatgpt.com/?fbclid=IwAR0eRtFjMuf5sESaa5W_PcBC3pgg-trMHTPYZi_FnH6QrhAlqKy_cXMDs70')}>FAQs</Text>
+        </Text>
+      </View>
     </ScrollView>
   );
 }
