679534da314b585078c374ca10b60b6f50381ceb
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-08-09 10:31:56 +0700

    Phần 2: Tạo cấu trúc cơ bản và thêm tiêu đề cho PrivacyPolicy

--- app/(outsidescreens)/PrivacyPolicy.tsx
+++ app/(outsidescreens)/PrivacyPolicy.tsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default function PrivacyPolicy() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Chính sách Bảo mật của BeeJobs</Text>
+    </ScrollView>
+  );
+}
