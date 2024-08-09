c82550d04c065de0d71480f477bc377af3f83562
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-08-09 10:27:06 +0700

    Phần 2: Tạo cấu trúc cơ bản và thêm tiêu đề cho HelpCenter

--- app/(outsidescreens)/HelpCenter.tsx
+++ app/(outsidescreens)/HelpCenter.tsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default function HelpCenter() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Trung tâm Trợ giúp BeeJobs</Text>
+    </ScrollView>
+  );
+}
