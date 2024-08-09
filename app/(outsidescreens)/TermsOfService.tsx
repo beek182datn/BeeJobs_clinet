1b7897d6040dd120f4b949344dd947dfaa7865c3
Bùi Duy Anh <113623522+Buiduyanh20@users.noreply.github.com>
2024-08-09 10:30:41 +0700

    Phần 2: Tạo cấu trúc cơ bản và thêm tiêu đề cho TermsOfService

--- app/(outsidescreens)/TermsOfService.tsx
+++ app/(outsidescreens)/TermsOfService.tsx
@@ -1,2 +1,10 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+
+export default function TermsOfService() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Điều khoản dịch vụ của BeeJobs</Text>
+    </ScrollView>
+  );
+}
