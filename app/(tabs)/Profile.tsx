4b86a65aa0ef51b18da578128e80239830f07326
huylvph30524 <vhuy887@gmail.com>
2024-08-22 00:18:35 +0700

    huy update

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -325,11 +325,11 @@ const Profile: React.FC = () => {
 
           <View style={styles.section}>
             <View style={styles.row}>
-              <View style={styles.infoBox}>
+              {/* <View style={styles.infoBox}>
                 <Ionicons name="eye" size={30} color="#0099CC" />
                 <Text style={styles.infoText}>NTD đã xem hồ sơ</Text>
                 <Text style={styles.infoNumber}>0</Text>
-              </View>
+              </View> */}
               <TouchableOpacity
                 style={styles.infoBox}
                 onPress={() => {
@@ -447,11 +447,11 @@ const Profile: React.FC = () => {
 
           <View style={styles.section}>
             <View style={styles.row}>
-              <View style={styles.infoBox}>
+              {/* <View style={styles.infoBox}>
                 <Ionicons name="eye" size={30} color="#0099CC" />
                 <Text style={styles.infoText}>NTD đã xem hồ sơ</Text>
                 <Text style={styles.infoNumber}>0</Text>
-              </View>
+              </View> */}
               <TouchableOpacity
                 style={styles.infoBox}
                 onPress={() => {
@@ -579,11 +579,11 @@ const Profile: React.FC = () => {
 
           <View style={styles.section}>
             <View style={styles.row}>
-              <View style={styles.infoBox}>
+              {/* <View style={styles.infoBox}>
                 <Ionicons name="eye" size={30} color="#0099CC" />
                 <Text style={styles.infoText}>NTD đã xem hồ sơ</Text>
                 <Text style={styles.infoNumber}>0</Text>
-              </View>
+              </View> */}
               <View style={styles.infoBox}>
                 <Ionicons name="business" size={30} color="#0099CC" />
                 <Text style={styles.infoText}>Công ty đang theo dõi</Text>
